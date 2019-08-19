b90e1052d4012e22257021ac66c912f26d71dde9
Adrian Sureshkumar <asureshkumar@scottlogic.com>
2019-08-19 12:14:06 +0100

    Render X3D element

--- src/donutChart3d.test.ts
+++ src/donutChart3d.test.ts
@@ -1,6 +1,28 @@
+import { select } from "d3";
+
 import donutChart3d from "./donutChart3d";
 
-it("should return a function", () => {
+it("should return a render function", () => {
     const chart = donutChart3d();
     expect(typeof chart).toBe("function");
+});
+
+describe("when the chart is rendered", () => {
+    let root: HTMLDivElement;
+
+    beforeAll(() => {
+        root = document.createElement("div")
+        document.body.append(root);
+
+        const chart = donutChart3d<HTMLDivElement, null, null, undefined>();
+        chart(select(root));
+    });
+
+    afterAll(() => {
+        root.remove();
+    });
+
+    it("should create an x3d element", () => {
+        expect(root.querySelector("x3d")).not.toBeNull();
+    })
 });
--- src/donutChart3d.ts
+++ src/donutChart3d.ts
@@ -1,6 +1,43 @@
-export default function donutChart3d(): () => void {
-    const my = function(): void {
+import { Selection, BaseType } from "d3";
+
+interface Render<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> {
+    (selection: Selection<GElement, Datum, PElement, PDatum>): void;
+}
+
+interface GetterSetter<T, V> {
+    (): V;
+    (value: V): T;
+}
+
+interface Series {
+    value: number;
+}
+
+interface RenderDonutChart3D<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> extends Render<GElement, Datum, PElement, PDatum> {
+    series: GetterSetter<this, Series[]>;
+}
+
+export default function donutChart3d<GElement extends BaseType, Datum, PElement extends BaseType, PDatum>(): RenderDonutChart3D<GElement, Datum, PElement, PDatum> {
+    let series: Series[] = [];
+
+    type Render = RenderDonutChart3D<GElement, Datum, PElement, PDatum>;
+    const render: Render = function(selection) {
+        const x3dDataJoin = selection.selectAll("x3d")
+            .data([series]);
+
+        x3dDataJoin.enter().append("x3d");
     };
 
-    return my;
+    function seriesGetterSetter(): Series[];
+    function seriesGetterSetter(value: Series[]): Render;
+    function seriesGetterSetter(value: Series[] | undefined = undefined): Series[] | Render {
+        if (value === undefined) return series;
+        series = value;
+        return render;
+    };
+
+    render.series = seriesGetterSetter;
+
+    return render;
 };
+
