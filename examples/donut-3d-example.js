a185179b0b108387774923472f76803bda59d63d
Adrian Sureshkumar <asureshkumar@scottlogic.com>
2019-08-20 14:50:54 +0100

    Specify height and width in example

--- examples/donut-3d-example.js
+++ examples/donut-3d-example.js
@@ -1,6 +1,8 @@
 const d3 = require("d3");
 const { donutChart3d } = require("../lib");
 
-const chart = donutChart3d();
+const chart = donutChart3d()
+    .height("600px")
+    .width("800px");
 
 d3.select("#chart-root").call(chart);
