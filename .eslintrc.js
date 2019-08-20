33ebb09ec38a9f9daac96f70a63b8f2223b5838d
Adrian Sureshkumar <asureshkumar@scottlogic.com>
2019-08-20 12:18:23 +0100

    Move ESLint and Jest config to package.json

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: "./tsconfig.json"
-    },
-    plugins: ["@typescript-eslint"],
-    extends: ["plugin:@typescript-eslint/recommended"]
-};
