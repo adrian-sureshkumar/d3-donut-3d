934a0696bec8ac7aa0f3c08316e71ddc2dd13a0b
Adrian Sureshkumar <asureshkumar@scottlogic.com>
2019-08-22 16:20:52 +0100

    Randomly update example chart data

--- examples/donut-3d-example.js
+++ examples/donut-3d-example.js
@@ -1,28 +1,42 @@
 const d3 = require("d3");
 const { donutChart3d } = require("../lib");
 
+const data = [{
+    color: "#FF0000",
+    name: "Red",
+    value: 1
+}, {
+    color: "#00FF00",
+    name: "Green",
+    value: 1
+}, {
+    color: "#0000FF",
+    name: "Blue",
+    value: 1
+}, {
+    color: "#FF00FF",
+    name: "Magenta",
+    value: 1
+}, {
+    color: "#FFFF00",
+    name: "Yellow",
+    value: 1
+}, {
+    color: "#00FFFF",
+    name: "Cyan",
+    value: 1
+}];
+
 const chart = donutChart3d()
-    .data([{
-        color: "#FF0000",
-        value: 45
-    }, {
-        color: "#00FF00",
-        value: 90
-    }, {
-        color: "#0000FF",
-        value: 135
-    }, {
-        color: "#FF00FF",
-        value: 10
-    }, {
-        color: "#FFFF00",
-        value: 60
-    }, {
-        color: "#00FFFF",
-        value: 20
-    }])
-    .labelFormat((name, value, percentage) => `${percentage.toFixed(0)}%`)
+    .data(data)
+    .labelFormat((name, value, percentage) => `${name} - ${percentage.toFixed(0)}%`)
     .height("600px")
     .width("800px");
 
-d3.select("#chart-root").call(chart);
+function render() {
+    data.forEach(datum => datum.value *= 1 + Math.random() / 4);
+    d3.select("#chart-root").call(chart);
+}
+
+render();
+d3.interval(render, 2000);
