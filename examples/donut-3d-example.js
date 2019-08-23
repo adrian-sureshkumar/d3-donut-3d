b14e21eacf462b06f6317148a99c299231270bb3
Adrian Sureshkumar <asureshkumar@scottlogic.com>
2019-08-23 16:21:15 +0100

    Use random data for example

--- examples/donut-3d-example.js
+++ examples/donut-3d-example.js
@@ -1,35 +1,18 @@
 const d3 = require("d3");
+const faker = require("faker");
 const { donutChart3d } = require("../lib");
 
-const data = [{
-    color: "#FF0000",
-    name: "Red",
+const scale = d3.scaleOrdinal(d3.schemeCategory10);
+
+const data = Array(10).fill(null).map((_, i) => ({
+    color: scale(i),
+    name: faker.commerce.product(),
     value: 1
-}, {
-    color: "#00FF00",
-    name: "Green",
-    value: 1
-}, {
-    color: "#0000FF",
-    name: "Blue",
-    value: 1
-}, {
-    color: "#FF00FF",
-    name: "Magenta",
-    value: 1
-}, {
-    color: "#FFFF00",
-    name: "Yellow",
-    value: 1
-}, {
-    color: "#00FFFF",
-    name: "Cyan",
-    value: 1
-}];
+}));
 
 const chart = donutChart3d()
     .data(data)
-    .labelFormat((name, value, percentage) => `${name} - ${percentage.toFixed(0)}%`)
+    .labelFormat((name, value, percentage) => `${name} (${percentage.toFixed(0)}%)`)
     .height("600px")
     .width("800px");
 
