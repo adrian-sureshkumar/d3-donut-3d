024a48a8e5a9f70b3b374eded219079431bbd47c
Adrian Sureshkumar <asureshkumar@scottlogic.com>
2019-09-06 15:57:40 +0100

    Extract props from chart

--- src/d3-utils/RenderFn.ts
+++ src/d3-utils/RenderFn.ts
@@ -1,5 +1,9 @@
 import { BaseType, Selection } from "d3";
 
-export interface RenderFn<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> {
+import { FluentD3GetSet } from "./FluentD3GetSet";
+
+export type RenderFn<GElement extends BaseType, Datum, PElement extends BaseType, PDatum, Props = {}> = {
     (selection: Selection<GElement, Datum, PElement, PDatum>): void;
+} & {
+    [Key in keyof Props]: FluentD3GetSet<RenderFn<GElement, Datum, PElement, PDatum, Props>, Props[Key]>
 }
--- src/donutChart3d.ts
+++ src/donutChart3d.ts
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import { Selection, BaseType, RGBColor, HSLColor } from "d3";
 
-import { FluentD3GetSet, makeFluentD3GetSet } from "./d3-utils/FluentD3GetSet";
+import { makeFluentD3GetSet } from "./d3-utils/FluentD3GetSet";
 import { RenderFn } from "./d3-utils/RenderFn";
 
 export interface DonutChart3dDatum {
@@ -14,14 +14,16 @@ export interface DonutChart3dLabelFormatter {
     (name: string, value: number, percentage: number): string;
 }
 
-export interface DonutChart3dRenderFn<GElement extends BaseType, PElement extends BaseType>
-    extends RenderFn<GElement, unknown, PElement, unknown> {
-    data: FluentD3GetSet<this, DonutChart3dDatum[]>;
-    height: FluentD3GetSet<this, string | null>;
-    labelFormat: FluentD3GetSet<this, DonutChart3dLabelFormatter | null>;
-    transitionDuration: FluentD3GetSet<this, number>;
-    width: FluentD3GetSet<this, string | null>;
+export interface DonutChart3dProps {
+    data: DonutChart3dDatum[];
+    height: string | null;
+    labelFormat: DonutChart3dLabelFormatter | null;
+    transitionDuration: number;
+    width: string | null;
 }
+
+export type DonutChart3dRenderFn<GElement extends BaseType, PElement extends BaseType> =
+    RenderFn<GElement, unknown, PElement, unknown, DonutChart3dProps>;
 
 const minorRadius = 0.5;
 const majorRadius = 1;
@@ -29,70 +31,77 @@ const outerRadius = majorRadius + minorRadius;
 
 const labelOffset = 1.5;
 
+interface ChartSeries {
+    color: RGBColor;
+    label: string;
+    sliceLength: number;
+    sliceStart: number;
+}
+
+function buildChartSeries({data, labelFormat}: DonutChart3dProps): ChartSeries[] {
+    const chartSeries: ChartSeries[] = [];
+
+    if (data.length) {
+        const cumulativeValues = [0];
+        data.forEach((datum, i) => cumulativeValues.push(cumulativeValues[i] + datum.value));
+
+        const total = cumulativeValues[cumulativeValues.length - 1];
+
+        const valueToAngleRatio = 2 * Math.PI / total;
+
+        data.forEach((datum, i) => {
+            const percentage = datum.value / total * 100;
+
+            const sliceStart = cumulativeValues[i] * valueToAngleRatio;
+            const sliceLength = datum.value * valueToAngleRatio;
+
+            const color = typeof datum.color === "string"
+                ? d3.rgb(datum.color)
+                : datum.color.rgb();
+
+            const label = labelFormat
+                ? labelFormat(datum.name || "", datum.value, percentage)
+                : "";
+
+            chartSeries.push({ sliceStart, sliceLength, color, label });
+        });
+    }
+
+    return chartSeries;
+}
+
 export function donutChart3d<GElement extends BaseType, PElement extends BaseType>(
 ): DonutChart3dRenderFn<GElement, PElement> {
-    let data: DonutChart3dDatum[] = [];
-    let height: string | null = null;
-    let labelFormat: DonutChart3dLabelFormatter | null = null;
-    let transitionDuration: number = d3.transition().duration();
-    let width: string | null = null;
+    const props: DonutChart3dProps = {
+        data: [],
+        height: null,
+        labelFormat: null,
+        transitionDuration: d3.transition().duration(),
+        width: null
+    }
 
-    const renderFn: DonutChart3dRenderFn<GElement, PElement> = 
-      selection => renderX3d(selection);
+    const renderFn: DonutChart3dRenderFn<GElement, PElement> =
+        selection => renderX3d(selection);
 
-    renderFn.data = makeFluentD3GetSet(renderFn, () => data, value => data = value);
-    renderFn.height = makeFluentD3GetSet(renderFn, () => height, value => height = value);
-    renderFn.labelFormat = makeFluentD3GetSet(renderFn, () => labelFormat, value => labelFormat = value);
-    renderFn.transitionDuration = makeFluentD3GetSet(renderFn, () => transitionDuration, value => transitionDuration = value);
-    renderFn.width = makeFluentD3GetSet(renderFn, () => width, value => width = value);
+    renderFn.data = makeFluentD3GetSet(
+        renderFn, () => props.data, value => props.data = value);
+    renderFn.height = makeFluentD3GetSet(
+        renderFn, () => props.height, value => props.height = value);
+    renderFn.labelFormat = makeFluentD3GetSet(
+        renderFn, () => props.labelFormat, value => props.labelFormat = value);
+    renderFn.transitionDuration = makeFluentD3GetSet(
+        renderFn, () => props.transitionDuration, value => props.transitionDuration = value);
+    renderFn.width = makeFluentD3GetSet(
+        renderFn, () => props.width, value => props.width = value);
 
     return renderFn;
 
-    interface ChartSeries {
-        color: RGBColor;
-        label: string;
-        sliceLength: number;
-        sliceStart: number;
-    }
-
-    function buildChartSeries(): ChartSeries[] {
-        const chartSeries: ChartSeries[] = [];
-
-        if (data.length) {
-            const cumulativeValues = [0];
-            data.forEach((datum, i) => cumulativeValues.push(cumulativeValues[i] + datum.value));
-
-            const total = cumulativeValues[cumulativeValues.length - 1];
-
-            const valueToAngleRatio = 2 * Math.PI / total;
-
-            data.forEach((datum, i) => {
-                const percentage = datum.value / total * 100;
-
-                const sliceStart = cumulativeValues[i] * valueToAngleRatio;
-                const sliceLength = datum.value * valueToAngleRatio;
-
-                const color = typeof datum.color === "string"
-                    ? d3.rgb(datum.color)
-                    : datum.color.rgb();
-
-                const label = labelFormat
-                    ? labelFormat(datum.name || "", datum.value, percentage)
-                    : "";
-
-                chartSeries.push({ sliceStart, sliceLength, color, label });
-            });
-        }
-
-        return chartSeries;
-    }
-
     function renderX3d(s: Selection<GElement, unknown, PElement, unknown>): void {
         s.selectAll("x3d")
-        .data([buildChartSeries()])
+        .data([buildChartSeries(props)])
         .join("x3d")
-          .style("height", `${height}`)
-          .style("width", `${width}`)
+          .style("height", `${props.height}`)
+          .style("width", `${props.width}`)
           .call(renderScene);
     }
 
@@ -118,7 +127,7 @@ export function donutChart3d<GElement extends BaseType, PElement extends BaseTyp
           .attr("class", "chart-series")
           .call(s => s
             .transition()
-              .duration(transitionDuration)
+              .duration(props.transitionDuration)
               .attr("rotation", d => `0 0 1 ${(Math.PI / 2) - d.sliceStart}`)
           )
           .call(renderChartSeriesSlices)
@@ -138,7 +147,7 @@ export function donutChart3d<GElement extends BaseType, PElement extends BaseTyp
               .attr("outerRadius", majorRadius) // Note: In X3DOM this actually defines the major radius.
               .attr("useGeoCache", false)
             .transition()
-              .duration(transitionDuration)
+              .duration(props.transitionDuration)
               .attr("angle", d => `${d.sliceLength}`)
           )
           .call(s => s
@@ -160,7 +169,7 @@ export function donutChart3d<GElement extends BaseType, PElement extends BaseTyp
           .attr("class", "chart-series-label")
           .call(s => s
             .transition()
-              .duration(transitionDuration)
+              .duration(props.transitionDuration)
               .attr("rotation", d => `0 0 1 ${-d.sliceLength / 2}`)
           )
         .call(renderChartSeriesLabelLines)
@@ -201,7 +210,7 @@ export function donutChart3d<GElement extends BaseType, PElement extends BaseTyp
           .attr("translation", `${outerRadius + labelOffset} 0 0`)
           .call(s => s
             .transition()
-              .duration(transitionDuration)
+              .duration(props.transitionDuration)
               .attr("rotation", d => `0 0 1 ${-(Math.PI / 2) + d.sliceStart + d.sliceLength / 2}`)
           )
         .selectAll("shape")
